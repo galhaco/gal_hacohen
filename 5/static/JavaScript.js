e31eb0f13170985719aa59ec194db32665af38b5
galhaco <galhaco@post.bgu.ac.il>
2023-05-07 13:17:15 +0300

    my fifth commit

---
+++ 5/static/JavaScript.js
@@ -0,0 +1,7 @@
+document.getElementById("demo").innerHTML= "hellow JavaScript!" ;
+console.log("hellow bob dylen");
+console.log("hellow SRV and Jimi Hendrix");
+var x;
+x=5.8;
+var y=7;
+console.log(x+y);
